e0407ab9d229ebeabfe7f8aca40e78116ec11220
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-06-05 12:57:01 -0600

    Complete index for invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,16 @@ Rails.application.routes.draw do
         get '/:id/invoice_items', to: 'invoice_items#index'
       end
 
+      namespace :invoice_items do
+        get '/', to: 'invoice_items#index'
+        # get '/find', to: 'find#show'
+        # get '/find_all', to: 'find#index'
+        # get '/random', to: 'random#show'
+        get '/:id', to: 'invoice_items#show'
+        # get '/:id/item', to: 'item#show'
+        # get '/:id/invoice', to: 'invoice#show'
+      end
+
       namespace :invoices do 
         get '/find_all', to: 'find#index'
         get 'find', to: 'find#show'
