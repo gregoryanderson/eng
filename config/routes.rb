e28735e16a0483957fbb746f67a1e43e444ccc50
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-06-05 12:42:34 -0600

    Complete index for invoice items of items

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
         get '/:id', to: 'items#show'
         # get '/:id/best_day', to: 'best_day#show'
         # get '/:id/merchant', to: 'merchant#show'
-        # get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/invoice_items', to: 'invoice_items#index'
       end
 
       namespace :invoices do 
---
+++ spec/requests/api/v1/items/items_invoice_items_index_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :request do
+  before(:each) do
+    @merchant = Merchant.create(name: "Greg", id: 1)
+    @item = Item.create(name: "Thing One", description: "Thing one is this thing", unit_price_in_cents: 12345, id: 1, merchant_id: 1)
+    @customer_1 = Customer.create(first_name: "Lucy", last_name: "Conklin", id: 1)
+    @customer_2 = Customer.create(first_name: "Bob", last_name: "Conklin", id: 2)
+    @customer_3 = Customer.create(first_name: "Carl", last_name: "Conklin", id: 3)
+    @invoice_1 = Invoice.create(merchant_id: 1, customer_id: 1, id: 1, status: "shipped")
+    @invoice_2 = Invoice.create(merchant_id: 1, customer_id: 2, id: 2, status: "shipped")
+    @invoice_3 = Invoice.create(merchant_id: 1, customer_id: 3, id: 3, status: "shipped")
+    @invoice_item_1 = InvoiceItem.create(item_id: 1, invoice_id: 1, id: 1, quantity: 1, unit_price_in_cents: 12345)
+    @invoice_item_2 = InvoiceItem.create(item_id: 1, invoice_id: 2, id: 2, quantity: 1, unit_price_in_cents: 12345)
+    @invoice_item_3 = InvoiceItem.create(item_id: 1, invoice_id: 3, id: 3, quantity: 1, unit_price_in_cents: 12345)
+  end
+
+  describe 'when I send a request to the items-invoice_items endpoint' do
+    before(:each) do
+      get "/api/v1/items/#{@item.id}/invoice_items"
+      @json = JSON.parse(response.body)
+    end
+
+    it 'I get JSON data for all invoice_items associated with item' do
+      expect(@json.class).to eq(Hash)
+      expect(@json.keys).to eq(['data'])
+      expect(@json['data'].class).to eq(Array)
+      expect(@json['data'].length).to eq(3)
+
+      expected = [@invoice_item_1, @invoice_item_2, @invoice_item_3]
+      @json['data'].each_with_index do |item, index|
+        expect(item['id']).to eq(expected[index].id.to_s)
+        expect(item['type']).to eq('invoice_item')
+
+        attributes = item['attributes']
+
+        formatted_price = sprintf('%.2f', expected[index].unit_price_in_cents / 100.0)
+        expect(attributes['id']).to eq(expected[index].id)
+        expect(attributes['item_id']).to eq(expected[index].item_id)
+        expect(attributes['invoice_id']).to eq(expected[index].invoice_id)
+        expect(attributes['quantity']).to eq(expected[index].quantity)
+        expect(attributes['unit_price']).to eq(formatted_price)
+      end
+    end
+  end
+end
